fcb413dad0e9e62ece8b836f35388366973eddfc
samraeng temram <elect_2510@hotmail.com>
2019-03-03 12:02:37 +0700

    add ht22

--- my_garden02.c
+++ my_garden02.c
@@ -64,18 +64,22 @@
 #define sw_down pin_e2
 #define sw_set pin_a5
 #define sw_prg pin_a4
-//--------------------------------------------------------------
+//---------------------ht22_io-----------------------------------
+#BIT Data_Pin = 0xf82.7                       // Pin mapped to PORTb.0
+#BIT Data_Pin_Direction = 0xf94.7             // Pin direction mapped to TRISc.3
+//---------------------------------------------------------------
 int8 trisb;
 #locate trisb = 0xf93
 
 int8 portb;
 #locate portb = 0xF81
+//#bit trisb0
 
-#bit light  = portb.0
-#bit value1 = portb.1
-#bit value2 = portb.2
-#bit value3 = portb.3
-#bit value4 = portb.4
+//#bit light  = portb.0
+#bit value1 = portb.0
+#bit value2 = portb.1
+#bit value3 = portb.2
+#bit value4 = portb.3
 
 int8 trisa;
 #locate trisa=0xf92
@@ -88,15 +92,54 @@ int8 trise;
 #bit    trise2=trise.2
 
 
+//---------------------------var for ht22-------------
+int8 temp100,temp10,temp0;
+int8 hum100,hum10,hum0;
+short Time_out ;
+unsigned int8 T_byte1, T_byte2, RH_byte1, RH_byte2, CheckSum ;
+unsigned int16 Temp, RH;
+void start_signal(){
+  Data_Pin_Direction = 0;              // Configure connection pin as output
+  Data_Pin = 0;                        // Connection pin output low
+  delay_ms(25);
+  Data_Pin = 1;                        // Connection pin output high
+  delay_us(30);
+  Data_Pin_Direction = 1;              // Configure connection pin as input
+}
+short check_response(){
+  delay_us(40);
+  if(!Data_Pin){                     // Read and test if connection pin is low
+    delay_us(80);
+    if(Data_Pin){                    // Read and test if connection pin is high
+      delay_us(50);
+      return 1;}
+    }
+}
+unsigned int8 Read_Data(){
+  unsigned int8 i, k, _data = 0;     // k is used to count 1 bit reading duration
+  if(Time_out)
+    break;
+  for(i = 0; i < 8; i++){
+    k = 0;
+    while(!Data_Pin){                          // Wait until pin goes high
+      k++;
+      if (k > 100) {Time_out = 1; break;}
+      delay_us(1);}
+    delay_us(30);
+    if(!Data_Pin)
+      bit_clear(_data, (7 - i));               // Clear bit (7 - i)
+    else{
+      bit_set(_data, (7 - i));                 // Set bit (7 - i)
+      while(Data_Pin){                         // Wait until pin goes low
+      k++;
+      if (k > 100) {Time_out = 1; break;}
+      delay_us(1);}
+    }
+  }
+  return _data;
+}
 
-//int8 trisc;
-//#locate trisc = 0xf94
-//#bit trisc7=trisc.7
-
-//int8 portc;
-//#locate portc=  0xf82
-//#bit pump = portc.7
-
+//--------------------------------------------------
 
 #define write_cntrl 0xd0
 #define read_cntrl  0xd1
@@ -129,7 +172,7 @@ signed int8 min00;
 signed int8 min10;
 //=========min on_off ==
 signed int8 min=30;
-
+int16 loop_display=0;
 
 int8 cmd;
 
@@ -206,10 +249,10 @@ trise2=1;
 
             // light =0;
              //pump  =0;
-             value1=0;
-             value2=0;
-             value3=0;
-             value4=0;
+             value1=1;
+             value2=1;
+             value3=1;
+             value4=1;
     
     IF(read_eeprom(0)==255 || read_eeprom(1)==255)
     {
@@ -233,8 +276,7 @@ trise2=1;
    
    
    lcd_init();
-  // lcd_gotoxy(1,1);
-  // lcd_putc("TIME V1");
+
    
   //  if(read_eeprom(26)==1) light=1;
 
@@ -242,7 +284,7 @@ trise2=1;
    
    while(true)
    {
-     //restart_wdt();
+     restart_wdt();
      read_time();
      //output_toggle(pin_e0);
      delay_ms(100);
@@ -255,16 +297,31 @@ trise2=1;
      
    // restart_wdt();
 
-
-      display_hours();
+   if(loop_display>=0&&loop_display<30)
+   {   loop_display++; 
+      lcd_gotoxy(1,1);
+       lcd_putc("TIME V1");   
+       display_hours();
       //=====================
       lcd_putc(":");
 
       display_min();
      //=======================
       display_sec();
-
- 
+   }//else{lcd_gotoxy(1,1);lcd_putc("        ");lcd_gotoxy(1,0);lcd_putc("        ");} 
+   if(loop_display>=30&&loop_display<=40)
+    {loop_display++;lcd_gotoxy(1,1);
+     lcd_putc("weather ");
+     lcd_gotoxy(1,0);lcd_putc(" & time ");
+    }
+   if(loop_display>=80&&loop_display<90)
+    {//loop_display=0;
+    loop_display++;lcd_gotoxy(1,1);
+     lcd_putc("weather ");
+     lcd_gotoxy(1,0);lcd_putc(" & time ");
+        
+    }
+    if(loop_display>=90)loop_display=0;
  //===============function set time=======
         if(!input(sw_set))
           {
@@ -286,8 +343,8 @@ trise2=1;
           
           } 
      
-              lcd_gotoxy(1,1);
-            lcd_putc("TIME V1");   
+           //   lcd_gotoxy(1,1);
+           // lcd_putc("TIME V1");   
             
  //========================= GET TABLE ===================            
      
@@ -344,11 +401,77 @@ trise2=1;
      {
       
      }
-   // if(pre_cmd != cmd)
-   // {
-   //  active_cmd();pre_cmd=cmd;
-   // } 
-    // chk_lcd();   
+//--------------------------------------     
+    Time_out = 0;
+    Start_signal();
+    if(check_response()){                    // If there is response from sensor
+      RH_byte1 = Read_Data();                 // read RH byte1
+      RH_byte2 = Read_Data();                 // read RH byte2
+      T_byte1 = Read_Data();                  // read T byte1
+      T_byte2 = Read_Data();                  // read T byte2
+      Checksum = Read_Data();                 // read checksum
+      if(Time_out){                           // If reading takes long time
+      
+        lcd_gotoxy(1,1); 
+        lcd_putc("        ");                       // LCD clear
+        lcd_gotoxy(1,0); 
+        lcd_putc("        ");                       // LCD clear  
+                
+        lcd_gotoxy(1,1);                     // Go to column 5 row 1
+        lcd_putc("Time out!");
+      }
+      else{
+       if(CheckSum == ((RH_Byte1 + RH_Byte2 + T_Byte1 + T_Byte2) & 0xFF)){
+        RH = RH_byte1;
+        RH = (RH << 8) | RH_byte2;
+        Temp = T_byte1;
+        Temp = (Temp << 8) | T_byte2;
+        if (Temp > 0X8000){
+        // message1[6] = '-';
+         Temp = Temp & 0X7FFF; }
+        else
+
+        
+        temp100 =  (Temp / 100) % 10 ;//////////////sam
+        temp10  = (Temp / 10) % 10 ;////////////////sam
+        temp0 = Temp % 10 ;////////////////////////sam
+                      
+        hum100 =  (RH / 100) % 10 ;
+        hum10 =  (RH / 10) % 10;
+        hum0  = RH % 10;
+        
+
+      if(loop_display>=40&&loop_display<70){loop_display++;     
+        lcd_gotoxy(1,1); lcd_putc("Temp");lcd_gotoxy(5, 1);        
+        lcd_putc(num[temp100]); lcd_putc(num[temp10]);lcd_putc("."); lcd_putc(num[temp0]); 
+  
+       lcd_gotoxy(1,0); lcd_putc("RH");lcd_gotoxy(3,0);lcd_putc(" ");
+       lcd_gotoxy(4, 0);
+       lcd_putc(num[hum100]); lcd_putc(num[hum10]);lcd_putc("."); lcd_putc(num[hum0]);  lcd_putc("%");      
+      }
+      if(loop_display>=70&&loop_display<80)
+      {loop_display++;lcd_gotoxy(1,1);
+     lcd_putc("weather ");
+     lcd_gotoxy(1,0);lcd_putc(" & time ");
+      
+      }
+      
+       }
+        else {
+          //lcd_putc('\f');                     // LCD clear
+          lcd_gotoxy(1, 1);                   // Go to column 1 row 1
+          lcd_putc("Checksum Error!");
+        }
+      }
+    }
+    else {
+      //lcd_putc('\f');             // LCD clear
+      //lcd_gotoxy(1, 0);           // Go to column 3 row 1
+      //lcd_putc("No response");
+      //lcd_gotoxy(1, 2);           // Go to column 1 row 2
+      //lcd_putc("from the sensor");
+    }   
+///////////////////////////////////////////    
      active_cmd();
    }
 }
@@ -450,102 +573,102 @@ switch (cmd) {
 
       case 1:
 
-             value1=1;
-             value2=0;
-             value3=0;
-             value4=0;
+             value1=0;
+             value2=1;
+             value3=1;
+             value4=1;
                   
 
            break;
 
       case 2:
        
-             value1=0;
-             value2=0;
-             value3=0;
-             value4=0;
+             value1=1;
+             value2=1;
+             value3=1;
+             value4=1;
            break;  
       case 3:
          
              value1=0;
              value2=1;
-             value3=0;
-             value4=0;
+             value3=1;
+             value4=1;
            break;            
            
       case 4:
        
-             value1=0;
-             value2=0;
-             value3=0;
-             value4=0;
+             value1=1;
+             value2=1;
+             value3=1;
+             value4=1;
            break;           
       case 5:
        
              value1=0;
-             value2=0;
-             value3=0;
+             value2=1;
+             value3=1;
              value4=1;    
 
            break;          
       case 6:
         
-             value1=0;
-             value2=0;
-             value3=0;
-             value4=0;
+             value1=1;
+             value2=1;
+             value3=1;
+             value4=1;
            break;          
       case 7:
            
              value1=1;
              value2=0;
-             value3=0;
-             value4=0;
+             value3=1;
+             value4=1;
            break;          
       case 8:
          
-             value1=0;
+             value1=1;
              value2=1;
-             value3=0;
-             value4=0;
+             value3=1;
+             value4=1;
            break;       
       case 9:
            
-             value1=0;
+             value1=1;
              value2=0;
              value3=1;
-             value4=0; 
+             value4=1; 
            break;            
           
       case 10:
          
-             value1=0;
-             value2=0;
-             value3=0;
+             value1=1;
+             value2=1;
+             value3=1;
              value4=1;
            break;
       case 11:
       
-             value1=0;
-             value2=0;
+             value1=1;
+             value2=1;
              value3=0;
-             value4=0; 
+             value4=1; 
            break;
       case 12:
        
-             value1=0;
-             value2=0;
-             value3=0;
-             value4=0;
+             value1=1;
+             value2=1;
+             value3=1;
+             value4=1;
            break;            
          
       default:
 
             
-             value1=0;
-             value2=0;
-             value3=0;
-             value4=0;
+             value1=1;
+             value2=1;
+             value3=1;
+             value4=1;
             break; }
 
 }
